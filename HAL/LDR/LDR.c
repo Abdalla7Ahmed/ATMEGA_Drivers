c1bde1fd59de09cacbc98185e4d8aaca25b42343
Abdalla7Ahmed <abdallahahmed17120@gmail.com>
2023-08-30 18:20:57 +0300

    LDR

---
+++ HAL/LDR/LDR.c
@@ -0,0 +1,30 @@
+﻿/*
+ * LDR.c
+ *
+ * Created: 30/08/2023 03:31:23 م
+ *  Author: GAMA
+ */ 
+#include "includes/LDR.h"
+
+void HAL_LDR_Init(void)
+{
+
+	
+	ADC_config_t ADC_PIN_CONFIG;
+	
+	ADC_PIN_CONFIG.ADC_Adjust_Result = ADC_Adjust_Result_RIGHT;
+	ADC_PIN_CONFIG.ADC_Auto_Trigger = ADC_Auto_Trigger_Enable;
+	ADC_PIN_CONFIG.ADC_Auto_Trigger_Source = ADC_Auto_Trigger_Source_Free_Running_mode;
+	ADC_PIN_CONFIG.ADC_Interrupt_Enable = ADC_Interrupt_Enable_CC;
+	ADC_PIN_CONFIG.ADC_Prescaler = ADC_Prescaler_128;
+	ADC_PIN_CONFIG.ADC_Voltage_ref = ADC_Voltage_ref_internal_2_56V;
+	ADC_PIN_CONFIG.Inpu_Channel = Inpu_Channel_Single_Ended_ADC0;
+	ADC_PIN_CONFIG.P_IRQ_Callback = HAL_LDR_Get_Value;
+	MCAL_ADC_init(&ADC_PIN_CONFIG);
+	
+		
+}
+uint16_t HAL_LDR_Get_Value(void)
+{
+	return MCAL_GET_ADC_Conversion();
+}
---
+++ HAL/LDR/LDR.h
@@ -0,0 +1,22 @@
+﻿/*
+ * LDR.h
+ *
+ * Created: 30/08/2023 03:31:31 م
+ *  Author: GAMA
+ */ 
+
+
+#ifndef LDR_H_
+#define LDR_H_
+#include "../../MCAL/Includes/DIO.h"
+#include "../../MCAL/Includes/ADC.h"
+#define LDR_Port									DIOC
+#define LDR_Pin										DIO_PIN_0
+
+
+void HAL_LDR_Init(void);
+uint16_t HAL_LDR_Get_Value(void);
+ 
+
+
+#endif /* LDR_H_ */
